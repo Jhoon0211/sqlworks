a020900803008d37bf2f32ab92903183abd19399
Jhoon0211 <ajh1070337@naver.com>
2023-04-12 15:08:33 +0900

    bookmall

---
+++ ex1.sql
@@ -0,0 +1,17 @@
+-- 문자 자료형 비교 <char vs varchar>
+CREATE TABLE ex1(
+    column1 CHAR(10),
+    column2 VARCHAR2(10)
+);
+
+-- 자료 추가
+INSERT INTO ex1(column1, column2) VALUES ('abc', 'abc');
+INSERT INTO ex1 VALUES ('강남', '강남');
+
+-- 자료 조회
+SELECT
+column1, LENGTH(column1) len1,
+column2, LENGTH(column2) len1
+FROM ex1;
+
+COMMIT;
