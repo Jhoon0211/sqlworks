7b2735a86ae75a3d67e15db5f3f43f628ce5d13a
Jhoon0211 <ajh1070337@naver.com>
2023-07-10 18:18:40 +0900

    mysqlworks

--- mydb/t_member.sql
+++ mydb/t_member.sql
@@ -47,6 +47,22 @@ SELECT * FROM t_board;
 SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result
 FROM t_member WHERE memberid = 'cloud';
 
+-- 게시판 페이지 처리
+-- .* 모든 속성 출력
+SELECT ROWNUM RN, t_board.* 
+FROM t_board
+WHERE ROWNUM >= 1 AND ROWNUM <= 10
+ORDER BY bnum;
+
+-- 페이지 처리 (인라인 뷰 - 중첩쿼리(서브쿼리))
+SELECT *
+FROM (SELECT ROWNUM RN, t_board.* 
+FROM t_board)
+WHERE RN >= 1 AND RN <= 10; -- 별칭(RN)을 사용해야 한다.
+-- ORDER BY bnum;
+
+-- 총 행의 수
+SELECT COUNT(*) FROM t_board;
 
 
 
