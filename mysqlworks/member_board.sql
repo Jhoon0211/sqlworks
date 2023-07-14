a3b87e559449eeeddd0d5d357c61d8da5536330f
Jhoon0211 <ajh1070337@naver.com>
2023-07-14 18:23:59 +0900

    member

--- mysqlworks/member_board.sql
+++ mysqlworks/member_board.sql
@@ -25,6 +25,22 @@ create table t_board(
     references t_member(memberid) on delete cascade
 );
 
+-- 댓글 추가
+insert into t_reply(bnum, rcontent, replyer)
+values(1, '내용에 대한 댓글입니다.', 'hibibo');
+
+-- 댓글 테이블
+create table t_reply(
+	rno int primary key auto_increment,		-- 댓글 번호
+    bnum int not null,						-- 게시글 번호
+    rcontent text not null,					-- 댓글 내용
+    replyer varchar(30) not null,			-- 작성자
+    rdate timestamp default now(),			-- 작성일
+    rupdate timestamp,						-- 수정일
+	constraint fk_reply_board foreign key(bnum)	-- 외래키 제약조건
+    references t_board(bnum) on delete cascade
+);
+
 
 -- 게시글 쓰기
 insert into t_board(title, content, memberid)
@@ -37,6 +53,20 @@ values('가입인사', '안녕하세요 가입했어요 루삥뽕', 'admin');
 -- 검색
 select * from t_member;
 select * from t_board ;
+select * from t_reply ;
+
 -- limit (시작, 끝)
 -- 0~10 : 1page, 11-20 : 2page
-select * from t_board order by bnum desc limit 0, 6;
+select * from t_board order by bnum desc l0imit 0, 6;
+
+-- 댓글 삭제
+delete from t_reply where rno = 1;
+
+-- 특정 댓글 검색
+select * from t_reply where rno = 3;
+
+-- 댓글 수정
+update t_reply set rcontent="내용수정"
+where rno= 3;
+
+commit;
