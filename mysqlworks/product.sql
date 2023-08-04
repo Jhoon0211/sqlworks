4b0a98954e047c611e5f88f6ee77d3ba0384ba60
Jhoon0211 <ajh1070337@naver.com>
2023-08-04 18:21:44 +0900

    board

--- mysqlworks/product.sql
+++ mysqlworks/product.sql
@@ -16,3 +16,6 @@ CREATE TABLE product(
 -- 상품 추가
 INSERT INTO product VALUES ('P1234', 'iPhone 6s', 1200000, 
 '1334X750 Retina HD Display', 'Smart Phone', 'Apple', 1000, 'New', 'P1234.png');
+
+DELETE from product where p_id = 'P1235';
+select * from product;
